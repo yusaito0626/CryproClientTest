d62e08805baab36485fd6d87559d6aa0df4cb591
yusaito0626 <104322591+yusaito0626@users.noreply.github.com>
2025-10-20 15:19:03 +0900

    Get WS_HOST from Envornment variables

--- Crypto_Linux/websocketServer.cs
+++ Crypto_Linux/websocketServer.cs
@@ -42,10 +42,13 @@ namespace Crypto_Linux
 
         public async Task StartAsync(CancellationToken token)
         {
-            _listener.Prefixes.Add("http://*:8080/");
+            string host = Environment.GetEnvironmentVariable("WS_HOST") ?? "http://127.0.0.1:8080/";
+            this.addLog("Host: " + host);
+            host = host.Trim('\"');
+            _listener.Prefixes.Add(host);
             _listener.Start();
-            this.addLog("WebSocket server started on ws://0.0.0.0:8080");
-            
+            this.addLog("WebSocket server started on " + host);
+
             while (!token.IsCancellationRequested)
             {
                 var context = await _listener.GetContextAsync();
